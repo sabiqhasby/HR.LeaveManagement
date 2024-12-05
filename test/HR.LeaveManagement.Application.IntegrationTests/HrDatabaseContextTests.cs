00c899f5502bf943acc6b928500715437ae48986
SabiqHasby <sabiqalhasby@gmail.com>
2024-12-05 12:07:48 +0700

    add Unit Test and Integration test

---
+++ test/HR.LeaveManagement.Application.IntegrationTests/HrDatabaseContextTests.cs
@@ -0,0 +1,57 @@
+﻿using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Shouldly;
+
+namespace HR.LeaveManagement.Application.IntegrationTests;
+
+public class HrDatabaseContextTests
+{
+    private HrDatabaseContext _hrDatabaseContext;
+
+    public HrDatabaseContextTests()
+    {
+        var dbOptions = new DbContextOptionsBuilder<HrDatabaseContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+        _hrDatabaseContext = new HrDatabaseContext(dbOptions);
+    }
+    [Fact]
+    public async Task Save_SetDateCreatedValue()
+    {
+        var leaveType = new LeaveType
+        {
+            Id = 1,
+            DefaultDays = 10,
+            Name = "Test Vacation"
+        };
+
+        //ACT
+
+        await _hrDatabaseContext.LeaveTypes.AddAsync(leaveType);
+        await _hrDatabaseContext.SaveChangesAsync();
+
+        //ASSERT
+        leaveType.DateCreated.ShouldNotBeNull();
+
+    }
+    [Fact]
+    public async Task Save_SetDateModifiedValue()
+    {
+        var leaveType = new LeaveType
+        {
+            Id = 1,
+            DefaultDays = 10,
+            Name = "Test Vacation"
+        };
+
+        //ACT
+
+        await _hrDatabaseContext.LeaveTypes.AddAsync(leaveType);
+        await _hrDatabaseContext.SaveChangesAsync();
+
+        //ASSERT
+        leaveType.DateModified.ShouldNotBeNull();
+
+    }
+}
---
+++ test/HR.LeaveManagement.Application.UnitTests/Mocks/MockLeaveTypeRepository.cs
@@ -0,0 +1,41 @@
+using System;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using Moq;
+
+namespace HR.LeaveManagement.Application.UnitTests.Mocks;
+
+public class MockLeaveTypeRepository
+{
+    public static Mock<ILeaveTypeRepository> GetLeaveTypeMockLeaveTypeRepository()
+    {
+        var leaveTypes = new List<LeaveType>{
+            new LeaveType {
+                Id = 1,
+                DefaultDays = 10,
+                Name = "Test Vacation"
+            },
+            new LeaveType {
+                Id = 2,
+                DefaultDays = 15,
+                Name = "Test Sick"
+            },
+            new LeaveType{
+                Id = 3,
+                DefaultDays = 15,
+                Name = "Test Maternity"
+            }
+        };
+
+        var mockRepo = new Mock<ILeaveTypeRepository>();
+
+        mockRepo.Setup(r => r.GetAsync()).ReturnsAsync(leaveTypes);
+        mockRepo.Setup(r => r.CreateAsync(It.IsAny<LeaveType>()))
+            .Returns((LeaveType lt) =>
+            {
+                leaveTypes.Add(lt);
+                return Task.CompletedTask;
+            });
+        return mockRepo;
+    }
+}
